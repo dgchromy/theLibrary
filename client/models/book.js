b9d8a7365d11570541748a29e7558b3f5d750cd9
Donald Chromy <dgchromy@gmail.comglobal user.emailgit config user.emailgit config --global user.email dgchromy@gmail.com>
2020-12-08 16:59:28 -0800

    added models folde and created book.js

---
+++ client/models/book.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const bookSchema = new Schema (
+    {
+        title: String,
+        author: String,
+        description: String, 
+        image: String, 
+        link: String,
+    }
+);
+
+const Book = mongoose.model("book", bookSchema);
+
+module.exports = Book; 
---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const routes = require("./routes");
+const app = express();
+const PORT = process.env.PORT || 3001;
+var http = require("http").Server(app);
+const io = require("socket.io")(http);
+
+// Define middleware here
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+
+// Define API routes here
+app.use(routes);
+
+// Connect to MongoDB //need Mongo db set up 
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/', { useNewUrlParser: true });
+
+//Connect to socket io
+io.on('connection', function(socket){
+  console.log("User connected");
+  socket.on('message', function(msg){
+    io.emit('message', msg);
+  });
+});
+
+app.listen(PORT, function() {
+  console.log(`🌎 ==> API server now on port ${PORT}!`);
+});
