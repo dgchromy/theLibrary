3b5ae41fd735be2cb2ae3dae9e02a17ee1bcaf45
Donald Chromy <dgchromy@gmail.comglobal user.emailgit config user.emailgit config --global user.email dgchromy@gmail.com>
2020-12-08 17:20:32 -0800

    created app.js function and set up routes to search saved and navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Search from "./pages/Search"
+import Saved from "./pages/Saved"
+import Navbar from "./components/navbar"
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router>
+            <div>
+                <Navbar />
+                <Route exact path="/" component={Search} />
+                <Route exact path="/search" component={Search} />
+                <Route exact path="/saved" component={Saved} />
+            </div>
+        </Router>
+  )
 }
 
 export default App;
